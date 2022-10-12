e83f58bcf6188734ceedaff1148762369aa12baf
Patryk Kaczmarek <patrykmkaczmarek@gmail.com>
2022-10-12 13:10:06 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+
+const api ={
+  key: 'bbcb421e22939803bcda1d7b42951a17',
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
+
+function App() {
+  return (
+    <div className='app'>
+      
+    </div>
+  );
+}
+
+export default App;
