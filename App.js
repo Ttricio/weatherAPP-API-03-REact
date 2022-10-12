43d00958ec04564a03d6eaf14bca82b743e59cc9
Patryk Kaczmarek <patrykmkaczmarek@gmail.com>
2022-10-12 15:12:21 +0200

    weather APP done

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const api = {
 	key: "bbcb421e22939803bcda1d7b42951a17",
 	base: "https://api.openweathermap.org/data/2.5/",
@@ -28,6 +30,22 @@ let daysArr = [
 ]
 
 function App() {
+	const [query, setQuery] = useState("")
+	const [weather, setWeather] = useState({})
+
+	const search = (event) => {
+		if (event.key === "Enter") {
+			fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`).then(
+				(res) =>
+					res.json().then((result) => {
+						setQuery("")
+						setWeather(result)
+						
+					})
+			)
+		}
+	}
+
 	const dateBuilder = (d) => {
 		let months = monthsArr
 		let days = daysArr
@@ -39,19 +57,39 @@ function App() {
 		return `${day} ${date} ${month} ${year}`
 	}
 	return (
-		<div className='app'>
+		<div
+			className={
+				typeof weather.main !== "undefined"
+					? weather.main.temp > 16
+						? "app warm"
+						: "app"
+					: "app"
+			}>
 			<main>
 				<div className='search-box'>
-					<input type='text' className='search-bar' placeholder='Search...' />
+					<input
+						type='text'
+						className='search-bar'
+						placeholder='Search...'
+						onChange={(e) => setQuery(e.target.value)}
+						value={query}
+						onKeyPress={search}
+					/>
 				</div>
-				<div className='location-box'>
-					<div className='location'>Warsaw, POL</div>
-					<div className='date'>{dateBuilder(new Date())}</div>
-				</div>
-				<div className='weather-box'>
-					<div className='temp'>15°C</div>
-					<div className='weather'> Sunny</div>
-				</div>
+				{typeof weather.main != "undefined" ? (
+					<div>
+						<div className='location-box'>
+							<div className='location'>{weather.name}</div>
+							<div className='date'>{dateBuilder(new Date())}</div>
+						</div>
+						<div className='weather-box'>
+							<div className='temp'>{Math.round(weather.main.temp)}°C</div>
+							<div className='weather'> {weather.weather[0].main}</div>
+						</div>
+					</div>
+				) : (
+					""
+				)}
 			</main>
 		</div>
 	)
