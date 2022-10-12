af0737e444c1e56f2ae2253b98c038630cf6ff0f
Patryk Kaczmarek <patrykmkaczmarek@gmail.com>
2022-10-12 13:21:55 +0200

    styling part 1

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
-
-const api ={
-  key: 'bbcb421e22939803bcda1d7b42951a17',
-  base: 'https://api.openweathermap.org/data/2.5/'
+const api = {
+	key: "bbcb421e22939803bcda1d7b42951a17",
+	base: "https://api.openweathermap.org/data/2.5/",
 }
 
 function App() {
-  return (
-    <div className='app'>
-      
-    </div>
-  );
+	return (
+		<div className='app'>
+			<main>
+				<div className='search-box'>
+					<input
+						type='text'
+						className='search-bar'
+						placeholder='Search...'></input>
+				</div>
+			</main>
+		</div>
+	)
 }
 
-export default App;
+export default App
